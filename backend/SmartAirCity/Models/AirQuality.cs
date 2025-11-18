dda5fb0974700cad60d7546e51aea2e36b73c1cb
Nguyen Nang Anh <nguyennanganh199203@gmail.com>
2025-11-18 16:19:43 +0700

    chore(license): update SPDX and copyright header in source files

--- backend/SmartAirCity/Models/AirQuality.cs
+++ backend/SmartAirCity/Models/AirQuality.cs
@@ -1,5 +1,6 @@
 //  SPDX-License-Identifier: MIT
 //  © 2025 SmartAir City Team
+ 
 //  This source code is licensed under the MIT license found in the
 //  LICENSE file in the root directory of this source tree.
 using MongoDB.Bson;
--- backend/SmartAirCity/Services/MqttSubscriberService.cs
+++ backend/SmartAirCity/Services/MqttSubscriberService.cs
@@ -1,3 +1,9 @@
+//  SPDX-License-Identifier: MIT
+//  © 2025 SmartAir City Team
+ 
+//  This source code is licensed under the MIT license found in the
+//  LICENSE file in the root directory of this source tree.
+
 using MQTTnet;
 using MQTTnet.Client;
 using MQTTnet.Extensions.ManagedClient;
